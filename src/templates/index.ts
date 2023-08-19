0533e9f1d0f76b0b62a85c2ad6d13ba74755eb39
Jeffrey Lewis <novensiles@gmail.com>
2023-08-19 18:10:46 +0800

    [BF-26] Create NPM package for contract templates

---
+++ src/templates/index.ts
@@ -0,0 +1,4 @@
+export * as CrowdSaleTemplate from './CrowdSale';
+export * as ERC20Template from './ERC20';
+export * as ERC721Template from './ERC721';
+export * as OneTimeSubscriptionTemplate from './OneTimeSubscription';
